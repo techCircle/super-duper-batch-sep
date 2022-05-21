597f414b446f104ed22e4a19644bacfd4ba8d749
techCircle <53327631+techCircle@users.noreply.github.com>
2022-05-21 13:54:12 -0400

    shafkat's 51 test cases in progress

---
+++ src/test/java/shafkat51TestCases/newClassToTest.java
@@ -0,0 +1,11 @@
+package shafkat51TestCases;
+
+public class newClassToTest {
+
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("hello 51 test cases,,,.. johny jogny yes papa");
+	}
+}
