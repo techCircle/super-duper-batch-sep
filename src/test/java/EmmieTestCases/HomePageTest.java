f322fa9dfb4290656b64bb85d7effbbcfdcab3e0
emmieniamnuam22 <emmieniamniam@gmail.com>
2022-06-01 16:44:55 -0400

    Emmie is Adding more test cases

--- src/test/java/EmmieTestCases/HomePageTest.java
+++ src/test/java/EmmieTestCases/HomePageTest.java
@@ -10,58 +10,76 @@ import Emmie_automationTestingPage.HomePage;
 import Emmie_automationTestingPage.ProductPage;
 import Emmie_automationTestingPage.ShopPage;
 
+
 public class HomePageTest {
 	HomePage hP = new HomePage();
 	ShopPage sP = new ShopPage();
 	ProductPage pD = new ProductPage();
+	
 
-	
-	
 	@BeforeClass
-	public void beforeTest(){
+	public void beforeTest() {
 		Emmie_Driver.getDriver();
 	}
-	
-	
+
 	@BeforeMethod
 	public void setUp() {
-		
+
 		hP.shopBtnClick();
 		sP.homeBtnClick();
 	}
 
 	@Test
-	public void homePagewiththreeSlidersonly() {
+	public void TC_001homePagewiththreeSlidersonly() {
 
 		hP.checkSliderCount();
 
 	}
 
 	@Test
-	public void homepagewiththreeArrivals() {
+	public void TC_002homepagewiththreeArrivals() {
 
 		hP.checkNewArrival();
 	}
 
 	@Test
-	public void homepageImagInArrivalsShouldNavigate() {
+	public void TC_003homepageImagInArrivalsShouldNavigate() {
 
 		hP.threeArrivalShouldNavigate();
 	}
 
 	@Test
-	public void ArrivalsImagesDescription() {
+	public void TC_004arrivalsImagesDescription() {
 		pD.navigateToDespription();
 	}
 
+	@Test
+	public void TC_005arrivalsImagesReviews() {
+		pD.reviewBtnClick();
+	}
+
+	@Test
+	public void TC_006arrivalsImagesAddtoBasket() {
+
+		pD.addBookToBasket();
+	}
+
+	@Test
+	public void TC_007AndTC_008arrivalsImagesAddtoBasketWithMoreBooks() {
+		pD.addMoreBooks();
+		pD.addBookToBasket();
+		pD.itemBtn.click();
+	}
+//	@Test
+//	public void TC_009arrivalsImagesAddtoBasketItemsCoupon() {
+////		bP.processCheckout.click();
+//	}
+
 	/*
-	 * Home page - Arrivals-Images-Description 5. Home page -
-	 * Arrivals-Images-Reviews 6. Home page - Arrivals-Images-Add to Basket 7. Home
-	 * page - Arrivals-Add to Basket with more books 8. Home-Arrivals-Add to
-	 * Basket-Items 9. Home-Arrivals-Add to Basket-Items-Coupon 10.
-	 * Home-Arrivals-Add to Basket-Items-Coupon value<450 11. Home-Arrivals-Add to
-	 * Basket-Items-Remove book 12. Home-Arrivals-Add to Basket-Items-Add book 13.
-	 * Home-Arrivals-Add to Basket-Items-Check-out-Book Final price 14.
+	 * Home-Arrivals-Add to Basket-Items 9. Home-Arrivals-Add to Basket-Items-Coupon
+	 * 10. Home-Arrivals-Add to Basket-Items-Coupon value<450 11. Home-Arrivals-Add
+	 * to Basket-Items-Remove book 12. Home-Arrivals-Add to Basket-Items-Add book
+	 * 13. Home-Arrivals-Add to Basket-Items-Check-out-Book Final price 14.
 	 * Home-Arrivals-Add to Basket-Items-Check-out-Update Basket 15.
 	 * Home-Arrivals-Add to Basket-Items-Check-out-Total & Sub-total condition 16.
 	 * Home-Arrivals-Add to Basket-Items-Check-out functionality 17.
@@ -70,7 +88,6 @@ public class HomePageTest {
 	 * 
 	 */
 
-	
 	@AfterClass
 	public void tearDown() {
 		Emmie_Driver.tearDown();
--- src/test/java/Emmie_automationTestingPage/ProductPage.java
+++ src/test/java/Emmie_automationTestingPage/ProductPage.java
@@ -1,5 +1,8 @@
 package Emmie_automationTestingPage;
 
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -8,20 +11,42 @@ import org.testng.Assert;
 import Emmie_Utilities.Emmie_Driver;
 
 public class ProductPage {
+	JavascriptExecutor js = (JavascriptExecutor) Emmie_Driver.driver;
+
 	public ProductPage() {
 		PageFactory.initElements(Emmie_Driver.getDriver(), this);
 	}
 
 	@FindBy(xpath = "//*[text()='Thinking in HTML']")
 	public WebElement clickThinkingInHTMLBook;
-	
+
 	@FindBy(xpath = "//*[text()='Description']")
 	public WebElement descriptionBtn;
 
 	@FindBy(xpath = "//*[@class='layoutArea']/div")
 	public WebElement description;
+
+	@FindBy(xpath = "//*[text()='Reviews (0)']")
+	public WebElement reviewBtn;
+
+	@FindBy(xpath = "//*[@class='comment-reply-title']")
+	public WebElement reviewText;
+
+	@FindBy(xpath = "//*[@id='product-163']/div[5]/ul/li")
+	public List<WebElement> clickBook;
+
+	@FindBy(xpath = "//*[@id='product-182']/div[2]/form/button")
+	public WebElement addBooksToBasket;
+
+	@FindBy(xpath = "//*[text()='HTML5 WebApp Develpment']")
+	public WebElement bookTitle;
+
+	@FindBy(xpath = "//*[@class='input-text qty text']")
+	public WebElement boxQty;
 	
-	
+	@FindBy(xpath = "//*[@class='wpmenucart-contents']")
+	public WebElement itemBtn;
+
 	public void navigateToDespription() {
 		clickThinkingInHTMLBook.click();
 		descriptionBtn.click();
@@ -32,6 +57,54 @@ public class ProductPage {
 		System.out.println(expected);
 		Assert.assertTrue(descp.contains(expected));
 
+	}
+
+	public void reviewBtnClick() {
+		clickThinkingInHTMLBook.click();
+		reviewBtn.click();
+
+		String review = reviewText.getText();
+		String expectedText = review;
+		System.out.println(review);
+		System.out.println(expectedText);
+		Assert.assertTrue(review.contains(expectedText));
+
+	}
+
+	public void addBookToBasket() {
+		clickThinkingInHTMLBook.click();
+		Emmie_Driver.clickProductFromList(clickBook, "HTML5");
+		addBooksToBasket.click();
+		String bookName = bookTitle.getText();
+		System.out.println("BOOK TITLE : " + bookName);
+		bookTitle.isDisplayed();
+	}
+
+	public void addMoreBooks() {
+		clickThinkingInHTMLBook.click();
+		Emmie_Driver.clickProductFromList(clickBook, "HTML5");
+		String bookName = bookTitle.getText();
+		System.out.println("BOOK TITLE : " + bookName);
+		bookTitle.isDisplayed();
+
+		String maxInput = boxQty.getAttribute("max");
+		System.out.println("Maximum quantity : " + maxInput);
+
+		int maxPlusOne = Integer.parseInt(maxInput + 1);
+
+		boxQty.clear();
+		boxQty.sendKeys(maxPlusOne + "");
+		// boxQty.sendKeys(String.valueOf(maxPlusOne));
+		addBooksToBasket.click();
+
+		js = (JavascriptExecutor) Emmie_Driver.getDriver();
+		Boolean isValidInput = (Boolean) js.executeScript("return arguments[0].checkValidity();", boxQty);
+		String validationMessage = (String) js.executeScript("return arguments[0].validationMessage;", boxQty);
+		System.out.println("Actual Alert : " + validationMessage);
+		Assert.assertFalse(isValidInput);
+		String alert = String.format("Value must be less than or equal to %d.", Integer.valueOf(maxInput));
+		Assert.assertTrue(alert.contains(validationMessage));
+		System.out.println("Expected Alert : " + alert);
 	}
 
 }
