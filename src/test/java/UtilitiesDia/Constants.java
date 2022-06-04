e1d5bd370c291869f7e12a9494f80ef676c6031d
srisubana <100324916+srisubana@users.noreply.github.com>
2022-06-04 14:03:35 -0400

    Adding my 51 test cases

---
+++ src/test/java/UtilitiesDia/Constants.java
@@ -0,0 +1,13 @@
+package UtilitiesDia;
+
+public class Constants {
+	
+	public static final String excelFilePath= "/Users/akkarapon/eclipse-workspace/Dia51.TestCases/src/test/resources/Excel/AkkaraponExcel.xlsx";
+	
+	public static final String config_filePath = System.getProperty("user.dir")+ "/src/test/resources/propertiesFolder/config.properties";
+	
+    public static final int implicitWaitTime =10;
+	
+	
+}
+
---
+++ src/test/java/UtilitiesDia/test.java
@@ -0,0 +1,11 @@
+package UtilitiesDia;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hell");
+
+	}
+
+}
